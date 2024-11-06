717cada9a6fb938825fcb63caab752f531b88bd4
mckenzielola <mckenzielola12@yahoo.com>
2024-11-06 14:48:04 -0800

    added user profile

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from . import forms # import from forms.py
 
 # Create your views here.
@@ -22,3 +23,7 @@ def register(request):
         # return back empty form, with password 
         form = forms.UserRegisterForm()
     return render(request, "users/register.html", {'form':form})
+
+@login_required()
+def profile(request):
+    return render(request, 'users/profile.html')
