fd1b1ef46deb5a70bf70bd014a159e9b0a1707f2
mckenzielola <mckenzielola12@yahoo.com>
2024-11-06 00:27:40 -0800

    create user login page and authenticator

--- users/views.py
+++ users/views.py
@@ -12,10 +12,10 @@ def register(request):
             # save form to backend
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"{username}, your account has been created!")
+            messages.success(request, f"{username}, your account has been created, please login")
 
-            # send user to home page using redirect
-            return redirect('ecowaste-home')
+            # send user to login using redirect
+            return redirect('user-login')
 
     # form is not valid 
     else:
