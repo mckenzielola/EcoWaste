5c4adb0175e8102edaf73ea4367a1890d69d84be
Nimastz <36112740+Nimastz@users.noreply.github.com>
2024-12-11 18:05:02 -0800

    update

--- ecowaste/models.py
+++ ecowaste/models.py
@@ -19,7 +19,7 @@ class WasteItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=50)
-    quantity = models.CharField(max_length=50)
+    quantity = models.PositiveIntegerField()
     date_added = models.DateField(auto_now_add=True)
 
     def __str__(self):
